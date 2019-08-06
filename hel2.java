26225cfb2d3ef47d173d38d55c7135774124be20
carrot <huluobu.cowork@gmail.com>
2019-08-06 05:51:18 +0000

    add new java file

---
+++ hel2.java
@@ -0,0 +1,9 @@
+public class basicbean {
+
+private String title;
+public void setusecache(bollean usercache){}
+public void setmaxsize(int size){}
+public string gettitle(){return title;}
+public  void settitle(string title){this.title=title;}
+
+}
