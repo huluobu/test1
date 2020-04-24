43471e6a0a73e67b1a1598156719b3b30eb2ba8f
carrot <huluobu.cowork@gmail.com>
2020-04-24 15:05:36 +0800

    自定义指定的服务提供者

--- testcloud/customerorder80/src/main/java/com/carrot/testcloud/OrderApplication.java
+++ testcloud/customerorder80/src/main/java/com/carrot/testcloud/OrderApplication.java
@@ -1,9 +1,11 @@
 package com.carrot.testcloud;
 
+import com.carrot.robin.MyRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 /**
  * create by: carrot
@@ -13,6 +15,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableEurekaClient
+@RibbonClient(name = "CLOUD-PROVIDER-PAYMENT",configuration = MyRule.class)
 public class OrderApplication {
     public static void main(String[] args) {
      SpringApplication.run(OrderApplication.class,args);}
