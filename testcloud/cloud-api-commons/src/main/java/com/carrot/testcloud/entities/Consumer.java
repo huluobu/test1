09a8d9297c6cbc1261e0c128f1852458c2f18a81
carrot <huluobu.cowork@gmail.com>
2020-08-24 10:59:38 +0800

    修改consumer文件

--- testcloud/cloud-api-commons/src/main/java/com/carrot/testcloud/entities/Consumer.java
+++ testcloud/cloud-api-commons/src/main/java/com/carrot/testcloud/entities/Consumer.java
@@ -1,6 +1,7 @@
 package com.carrot.testcloud.entities;
 
 import java.io.Serializable;
+import java.util.Date;
 
 /**
  * @Author: carrot
@@ -11,4 +12,6 @@ public class Consumer implements Serializable {
     private  long id;
     private String address;
     private  String iden;
+    private String age;
+    private Date birth;
 }
