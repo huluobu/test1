e4e10ef37d670234675ae88c23d11144bd85142d
carrot <huluobu.cowork@gmail.com>
2020-08-24 11:09:11 +0800

    新增身高字段

--- testcloud/cloud-api-commons/src/main/java/com/carrot/testcloud/entities/Consumer.java
+++ testcloud/cloud-api-commons/src/main/java/com/carrot/testcloud/entities/Consumer.java
@@ -12,5 +12,7 @@ public class Consumer implements Serializable {
     private  long id;
     private String address;
     private  String iden;
-	private  int age;	private Date birth;
+	private  int age;
+	private Date birth;
+	private String weigth;
 }
